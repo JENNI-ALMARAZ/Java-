c5420afa454fcc1f0e01d614b229feadd32a7e9a
JENNI-ALMARAZ <91512122+JENNI-ALMARAZ@users.noreply.github.com>
2021-10-05 16:06:30 -0500

    Hola munod en Java
    Se presenta un Hola mundo en lenguaje Java, ejecutado y compilado desde línea de comandos de vim

---
+++ Hola_mundo_java.java
@@ -0,0 +1,18 @@
+public class Hola_mundo_java {
+	   /**
+	    * Aplicación java 
+	    * Title	   :Hola mundo en java 
+	    * Description  :Hola mundo en java desde vim en windows
+	    * Author	   :Jennifer Almaraz Suárez 
+	    * Date	   :20211005
+	    * Version 	   :1
+	    * Usage	   :C:\Users\Jennifer Almaraz\OneDrive\Documentos\HOLA MUNDOS
+	    *	 	   :java>Hola_mundo_java.java
+	    *
+	    * Hola mundo en java 
+	    */
+
+	   public static void main(String []args) {
+	   		 System.out.println("Hola mundo desde java usando línea de comandos de vim"); //prints Hola mundo en java  
+	   } 		 
+} 
